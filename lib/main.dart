f14b8f4eba04bd8c0278ac8b838e9a536f4e5f53
Alysson Eduardo <alysson6801@hotmail.com>
2020-10-19 21:56:38 -0300

    (19/10) Implementação do Remover e desfazer.

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  TextEditingController toDo = TextEditingController();
+  final TextEditingController toDo = TextEditingController();
 
   List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
 
   //lendo dados salvos, necessário ler um método já existente
@@ -82,29 +85,67 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
-                itemBuilder: (context, index){
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"]?
-                        Icons.check: Icons.error
-                      )
-                    ),
-                    onChanged: (c){
-                      setState(() {
-                        _toDoList[index]["ok"] = c;
-                        _saveData();
-                      });
-                    },
-                  );
-                }),
+                itemBuilder: itemBuilder),
           )
         ]
       ),
     );
   }
 
+ /* */
+  //Item Builder
+  Widget itemBuilder(context, index){
+    //Dismissible, arrastavel
+    return Dismissible(
+      //Atribui a key apartir da hora em milisegundos
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete, color: Colors.white),
+          ),
+        ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"]?
+            Icons.check: Icons.error
+            )
+        ),
+        onChanged: (c){
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+        });
+
+
+        final snack = SnackBar(
+          content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+          action: SnackBarAction(label: "Desfazer",
+            onPressed: (){
+            _toDoList.insert(_lastRemovedPos, _lastRemoved);
+            _saveData();
+            },
+          ),
+          duration: Duration(seconds: 2),
+        );
+        Scaffold.of(context).showSnackBar(snack);
+      },
+    );
+  }
   //Função para buscar o arquivo
   Future<File> _getFile() async{
     //Faz a requisição do diretório padrão
